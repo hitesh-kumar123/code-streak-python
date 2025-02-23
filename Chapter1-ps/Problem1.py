da15cb0cb2d5d2f6806514ed1da04bf52cacbfed
hitesh-kumar123 <hiteshdevkumar2003+github@gmail.com>
2025-02-23 22:17:01 +0530

    firstproblem

---
+++ Chapter1-ps/Problem1.py
@@ -0,0 +1,55 @@
+print('''[Verse 1]
+I was in a sheer dress the day that we met
+We were both in a rush, we talked for a sec
+Your friend hit me up so we could connect
+And what are the odds? You send me a text
+And now the next thing I know, I'm like
+Manifest that you're oversized
+I digress, got me scrollin' like
+Out of breath, got me goin' like
+
+[Pre-Chorus]
+Ooh (Ah)
+Who's the cute boy with the white jacket and the thick accent? Like
+Ooh (Ah)
+Maybe it's all in my head
+
+[Chorus]
+But I bеt we'd have really good bеd chem
+How you pick me up, pull 'em down, turn me 'round
+Oh, it just makes sense
+How you talk so sweet when you're doin' bad things
+That's bed (Bed) chem (Chem)
+How you're lookin' at me, yeah, I know what that means
+And I'm obsessed
+Are you free next week?
+I bet we'd have really good
+      [Verse 2]
+Come right on me, I mean camaraderie
+Said you're not in my timezone, but you wanna be
+Where art thou? Why not uponeth me?
+See it in my mind, let's fulfill the prophecy
+
+[Pre-Chorus]
+Ooh (Ah)
+Who's the cute guy with the wide, blue eyes and the big bad mm? Like
+Ooh (Ah)
+I know I sound a bit redundant
+
+[Chorus]
+But I bet we'd have really good bed chem
+How you pick me up, pull 'em down, turn me 'round
+Oh, it just makes sense (Oh, it just makes sense)
+How you talk so sweet when you're doin' bad things (Bad)
+That's bed chem (Oh, that's bed chem)
+How you're lookin' at me, yeah, I know what that means
+And I'm obsessed (So obsessed)
+Are you free next week?
+I bet we'd have really good (Bed chem)
+
+[Bridge]
+And I bet we'd both arrive at the same time (Bed chem)
+And I bet the thermostat's set at six-nine (Bed-bed ch-chem)
+And I bet it's even better than in my head (My)
+
+''')
