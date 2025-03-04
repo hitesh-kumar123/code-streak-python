5853867c72de7ea44b62d875740f17eec1ec9001
hitesh-kumar123 <hiteshdevkumar2003@gmail.com>
2025-03-04 23:46:07 +0530

    update

---
+++ Chapter2/04_operators.py
@@ -0,0 +1,20 @@
+#Arithmetic Operators
+a=3
+b=4
+c=a+b
+# print(c)
+
+# Assignment Operators
+a = 4-2 # Assign 4-2 in a
+print (a)
+b=6
+b+=3 # Increment  the value  of by  3  and the assign it to b
+b-=3# Increment  the value  of by  3  and the assign it to b
+# print(b)
+
+# comparison Operators
+
+d = 5>4
+print(d)
+
+# LogicalOperators
