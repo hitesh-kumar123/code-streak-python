07cd8075b1802e623b444b9acdbe99905a985190
hitesh-kumar123 <hiteshdevkumar2003@gmail.com>
2025-03-12 21:57:37 +0530

    update

---
+++ Chapter4/01_list.py
@@ -0,0 +1,15 @@
+friends = ["Apple","Orange",5,345.06,False, "Aakash","Rohan"]
+
+print(friends[0])
+friends[0]="Grapes" 
+
+print(friends[0])
+print(friends[1:4])
+
+l1 = [1,34,62,2,6,11]
+# l1.sort()
+# l1.reverse()
+# l1.insert(2,33333)
+value = l1.pop(3)
+print(value)
+print(l1)
