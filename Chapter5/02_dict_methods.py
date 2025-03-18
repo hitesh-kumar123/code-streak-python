dd3096522c1d50bc4f6fc58e04d7f7ddde1f8903
hitesh-kumar123 <hiteshdevkumar2003@gmail.com>
2025-03-18 22:12:08 +0530

    update

---
+++ Chapter5/02_dict_methods.py
@@ -0,0 +1,13 @@
+marks = {
+    "Hitesh": 9,
+    "sumabm": 70,
+    "Kuresi": 80,
+}
+# print(marks.items())
+# print(marks.keys())
+# print(marks.values())
+# marks.update({"Hitesh":100, "Renuka":99})
+# print(marks)
+# print(marks.get("Hitesh1")) #Prints None
+# print(marks["Hitesh1"]) # Returns an error
+print(marks.pop("Hitesh", None))
