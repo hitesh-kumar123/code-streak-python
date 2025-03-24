d8ff2458916df117f96554abfb343a2c3623185d
hitesh-kumar123 <hiteshdevkumar2003@gmail.com>
2025-03-25 00:04:56 +0530

    update

---
+++ Chapter5-PS/04_problem4.py
@@ -0,0 +1,8 @@
+s = set()
+s.add(20)
+s.add(20.0)
+s.add('20') # length of s after these operations?
+print(len(s))
+
+# interview question IMP
+# python me 1=1.0 hoto python ka answer true hota becasue python me data type matter nhi karte he(jab value same ho tab)
