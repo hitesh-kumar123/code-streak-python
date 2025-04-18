0ade377b3029137681883d1baf1a2cbd2d82cbd8
hitesh-kumar123 <hiteshdevkumar2003@gmail.com>
2025-04-18 23:57:20 +0530

    update

---
+++ Chapter6-PS/05_problem5.py
@@ -0,0 +1,9 @@
+l = ["Harry", "Hitesh", "monisha", "kapila" ]
+
+name = input("Enter your name: ")
+
+if(name in l):
+    print("your name is in the list")
+
+else:
+    print("your name is not in the list")
